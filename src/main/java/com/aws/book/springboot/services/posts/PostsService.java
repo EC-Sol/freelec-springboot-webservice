c9556a6ab96f4b5d43676903e828fe972e5d45a2
EC-Sol <yhs1203@gmail.com>
2020-06-24 17:59:33 +0900

    게시글 수정 삭제 기능 추가

--- src/main/java/com/aws/book/springboot/services/posts/PostsService.java
+++ src/main/java/com/aws/book/springboot/services/posts/PostsService.java
@@ -2,8 +2,12 @@ package com.aws.book.springboot.services.posts;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.aws.book.springboot.domain.posts.Posts;
 import com.aws.book.springboot.domain.posts.PostsRepository;
+import com.aws.book.springboot.web.dto.PostsListResponseDto;
 import com.aws.book.springboot.web.dto.PostsResponseDto;
 import com.aws.book.springboot.web.dto.PostsSaveRequestDto;
 import com.aws.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -29,6 +33,21 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+            .map(PostsListResponseDto::new)
+            .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+
+        postsRepository.delete(posts);
     }
 
     public PostsResponseDto findById(Long id) {
--- src/main/java/com/aws/book/springboot/web/PostsApiController.java
+++ src/main/java/com/aws/book/springboot/web/PostsApiController.java
@@ -9,6 +9,7 @@ import com.aws.book.springboot.web.dto.PostsResponseDto;
 import com.aws.book.springboot.web.dto.PostsSaveRequestDto;
 import com.aws.book.springboot.web.dto.PostsUpdateRequestDto;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,14 +28,21 @@ public class PostsApiController {
         return postsService.save(requestDto);
     }
 
-    // @PutMapping("/api/v1/posts/{id}")
-    // public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
-    //     return postsService.update(id, requestDto);
-    // }
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
     
     
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById (@PathVariable Long id) {        
         return postsService.findById(id);
     }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+    
 }
